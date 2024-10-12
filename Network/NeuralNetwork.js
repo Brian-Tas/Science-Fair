79f8fce1a11f6fcf598fd8d5d0f6cd3848c40c48
Brian-Tas <brianjtascone@gmail.com>
2024-10-11 21:29:29 -0400

    Made basic layer structure

--- Network/NeuralNetwork.js
+++ Network/NeuralNetwork.js
@@ -11,4 +11,14 @@ class Level {
     }
 }
 
-console.log(new Level(5, 5));
+class NeuralNetwork {
+    constructor(neuronCount /* neurons per level; array */) {
+        this.levels = []
+
+        for(let i = 0; i < neuronCount.length - 1; i++) {
+            this.levels.push(new Level(neuronCount[i], neuronCount[i]));
+        }
+    }
+}
+
+console.log(new NeuralNetwork([5, 5]));
