e375ada3cff363a6dd500202710ca8843516affe
Brian-Tas <brianjtascone@gmail.com>
2024-10-11 23:02:26 -0400

    Update NeuralNetwork.js

--- Network/NeuralNetwork.js
+++ Network/NeuralNetwork.js
@@ -5,20 +5,39 @@ class Level {
         this.outputs = new Array(outputCount);
 
         this.weights = [];
-        for(let i = 0; i < outputCount; i++) {
+        for(let i = 0; i < inputCount; i++) {
             this.weights[i] = new Array(outputCount);
+        }
+
+        Level.randomize(this);
+    }
+
+    static randomize(level) {
+        for(let i = 0; i < level.biases.length; i++) {
+            level.biases[i] = (Math.random() * 2 - 1)
         }
     }
 }
 
 class NeuralNetwork {
     constructor(neuronCount /* neurons per level; array */) {
-        this.levels = []
+        this.levels = [];
 
-        for(let i = 0; i < neuronCount.length - 1; i++) {
-            this.levels.push(new Level(neuronCount[i], neuronCount[i]));
-        }
+        this.levels.push(new Level(
+            5, //2 sensors, speed, turn speed, bias 1
+            7
+        ));
+        this.levels.push(new Level(
+            7, 7
+        ));
+        this.levels.push(new Level(
+            7, 7
+        ));
+        this.levels.push(new Level(
+            7,
+            4 //left, right, forward, back
+        ))
     }
 }
 
-console.log(new NeuralNetwork([5, 5]));
+console.table(new NeuralNetwork().levels);
